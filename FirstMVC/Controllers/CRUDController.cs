8bd1b271b160e0ff25ae68cfa91b552c95842c85
galihanggara68 <galihanggara68@gmail.com>
2020-03-04 17:04:56 +0700

    + Added Entity Manager With CRD

---
+++ FirstMVC/Controllers/CRUDController.cs
@@ -0,0 +1,66 @@
+﻿using FirstMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class CRUDController : Controller
+    {
+        // GET: CRUD
+        [HttpGet]
+        [Route("employees")]
+        public ActionResult Index()
+        {
+            using(HREntities hr = new HREntities())
+            {
+                var employees = hr.COPY_EMP.ToList();
+                return View(employees);
+            }
+        }
+
+        [Route("employees/{id}")]
+        public ActionResult GetOneEmployee(int id)
+        {
+            using(HREntities hr = new HREntities())
+            {
+                var employee = hr.COPY_EMP.Find(id);
+                return View("Employee", employee);
+            }
+        }
+
+        [HttpGet]
+        [Route("employees/add")]
+        public ActionResult EmployeeForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("employees/add")]
+        public ActionResult PostEmployee(COPY_EMP emp)
+        {
+            using(HREntities hr = new HREntities())
+            {
+                hr.COPY_EMP.Add(emp);
+                hr.SaveChanges();
+                return Redirect("~/employees");
+            }
+        }
+
+        [HttpGet]
+        [Route("employees/{employeeId}/delete")]
+        public ActionResult DeleteEmployee(int employeeId)
+        {
+            using(HREntities hr = new HREntities())
+            {
+                var employee = hr.COPY_EMP.Find(employeeId);
+                hr.COPY_EMP.Remove(employee);
+                hr.SaveChanges();
+                return Redirect("~/employees");
+            }
+        }
+    }
+}
--- FirstMVC/Models/Person.cs
+++ FirstMVC/Models/Person.cs
@@ -10,11 +10,13 @@ namespace FirstMVC.Models
     {
         public int Id { get; set; }
 
-        [Required]
-        [StringLength(10)]
+        [Required(ErrorMessage = "Nama harus diisi")]
+        [StringLength(10, MinimumLength = 3)]
         public string Name { get; set; }
 
         [Required]
+        [MaxLength(15, ErrorMessage = "Alamat terlalu panjang")]
+        [MinLength(5)]
         public string Address { get; set; }
     }
 }
