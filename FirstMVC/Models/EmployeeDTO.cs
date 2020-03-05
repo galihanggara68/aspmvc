3e621d54419e280acc0a816ea369737dd6274eff
galihanggara68 <galihanggara68@gmail.com>
2020-03-05 15:36:42 +0700

    + Added EmployeeDTO

---
+++ FirstMVC/Models/EmployeeDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstMVC.Models
+{
+    public class EmployeeDTO
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime HiredDate { get; set; }
+        public long Salary { get; set; }
+    }
+}
